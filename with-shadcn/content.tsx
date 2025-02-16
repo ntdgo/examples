20b04ca4363d89dd3fb9ca3ccc508ab1943195e8
NTDGO <thanhdat6716@gmail.com>
2025-02-16 12:10:56 +0900

    injectTailwind

--- with-shadcn/content.tsx
+++ with-shadcn/content.tsx
@@ -1,5 +1,6 @@
 import cssText from "data-text:~globals.css"
 import type { PlasmoCSConfig } from "plasmo"
+import { createRoot } from "react-dom/client"
 
 import { LoginForm } from "~components/login-form"
 
@@ -38,6 +39,30 @@ export const getStyle = (): HTMLStyleElement => {
 }
 
 const CSUIExample = () => {
+  const containerBox = () => {
+    const container = document.createElement("div")
+    const root = createRoot(container)
+    root.render(
+      <div
+        className={`h-[300px] w-full bg-slate-50 text-black dark:bg-slate-900 dark:text-white`}>
+        <p className="text-center text-2xl text-red-500">container box</p>
+      </div>
+    )
+    return container
+  }
+
+  function injectTailwind() {
+    const nameElement = document.getElementById("hero-section-brand-heading")
+    if (nameElement) {
+      const textGroupParent = nameElement.parentElement
+      if (textGroupParent) {
+        textGroupParent.insertBefore(containerBox(), textGroupParent.firstChild)
+      }
+    }
+  }
+
+  injectTailwind()
+
   return (
     <div className="flex w-[400px] flex-col border-2 bg-yellow-50">
       <LoginForm />
